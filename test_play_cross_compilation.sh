308dabe48f6e4aa65c782886f9881a61e3627c12
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-05-10 14:09:10 +0100

    BAU Replace PlayCrossCompilation with standard multi-module

--- test_play_cross_compilation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-export PLAY_VERSION="2.5"
-sbt "; version; show libraryDependencies; clean; test"
-
-export PLAY_VERSION="2.6"
-sbt "; version; show libraryDependencies; clean; test"
